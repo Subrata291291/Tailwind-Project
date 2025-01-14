c9754adc9d8e9c2fcfba771676c3696fb5ae7902
Subrata291291 <subrata.haldar@codeclouds.com>
2025-01-14 19:47:12 +0530

    Added Form action page

--- dist/thank-you.php
+++ dist/thank-you.php
@@ -7,6 +7,7 @@
     $guest = $_POST['guest'];
     $events = $_POST['events'];
     $notes = $_POST['notes'];
+
     $subject = "Form submission";
     $subject2 = "Copy of your form submission";
     $message = "Name:".$fullname . "\n\n". " Guest:"  . $guest . "\n\n". "  Events:" .  $events . "\n\n". "  Phone No.:" .  $phone . "\n\n". "  Comments:"    . $_POST['notes'];
@@ -23,50 +24,3 @@
             }
 
             ?>
-
-
-            
-
-<?php  
- 
-if(isset($_POST['submit'])) {
- $mailto = "subrata@a1future.com";  //My email address
- //getting customer data
- $fullname = $_POST['Name']; //getting customer name
- $fromEmail = $_POST['your-email']; //getting customer email
- $phone = $_POST['phone']; //getting customer Phome number
- $notes = $_POST['notes']; //getting subject line from client
- $subject2 = "Confirmation: Message was submitted successfully | HMA WebDesign"; // For customer confirmation
- 
- //Email body I will receive
- $message = "Cleint Name: " . $name . "\n\n"
- . "Phone Number: " . $phone . "\n\n"
- . "Email Address: " . $fromEmail . "\n\n"
- . "Client Message: " . $_POST['query'];
- 
- //Message for client confirmation
- $message2 = "Dear" . $name . "\n"
- . "Thank you for contacting us. We will get back to you shortly!" . "\n\n"
- . "You submitted the following message: " . "\n" . $_POST['notes'] . "\n\n"
- . "Regards," . "\n" . "- HMA WebDesign";
- 
- //Email headers
- $headers = "From: " . $fromEmail; // Client email, I will receive
- $headers2 = "From: " . $mailto; // This will receive client
- 
- //PHP mailer function
- 
-  $result1 = mail($mailto, $subject, $message, $headers); // This email sent to My address
-  $result2 = mail($fromEmail, $subject2, $message2, $headers2); //This confirmation email to client
- 
-  //Checking if Mails sent successfully
- 
-  if ($result1 && $result2) {
-    $success = "Thank You...";
-  } else {
-    $failed = "Sorry! Message was not sent, Try again Later.";
-  }
- 
-}
- 
-?>
