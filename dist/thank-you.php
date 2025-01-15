2ae80b3b057fdf85ca7dba8c525a39ff55bc32ef
Subrata291291 <subrata.haldar@codeclouds.com>
2025-01-15 15:28:30 +0530

    Added thank-you.php file

--- dist/thank-you.php
+++ dist/thank-you.php
@@ -1,26 +1,21 @@
-<?php
-     if(isset($_POST["submit"])){
-    $to = "subrata291291@gmail.com"; // this is your Email address
+<?php 
+if(isset($_POST['submit'])){
+    $to = "subrata.haldar@codeclouds.com"; // this is your Email address
     $from = $_POST['email']; // this is the sender's Email address
-    $fullname = $_POST['name'];
-    $phone=$_POST["phone"];
+    $full_name = $_POST['name'];
     $guest = $_POST['guest'];
     $events = $_POST['events'];
-    $notes = $_POST['notes'];
-
+    $events = $_POST['notes'];
     $subject = "Form submission";
     $subject2 = "Copy of your form submission";
-    $message = "Name:".$fullname . "\n\n". " Guest:"  . $guest . "\n\n". "  Events:" .  $events . "\n\n". "  Phone No.:" .  $phone . "\n\n". "  Comments:"    . $_POST['notes'];
-    $message2 = "Here is a copy of your message " . $fullname . "\n\n" . $_POST['notes'];
+    $message = $full_name . " " . $guest . " wrote the following:" . "\n\n" . $_POST['message'];
+    $message2 = "Here is a copy of your message " . $full_name . "\n\n" . $_POST['message'];
 
     $headers = "From:" . $from;
     $headers2 = "From:" . $to;
     mail($to,$subject,$message,$headers);
-    //mail($from,$subject2,$message2,$headers2); 
-
-
-          echo'<script>alert("Message sent successfully"</script>';
-
-            }
-
-            ?>
+    mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
+    echo "Mail Sent. Thank you " . $full_name . ", we will contact you shortly.";
+    // You can also use header('Location: thank_you.php'); to redirect to another page.
+    }
+?>
